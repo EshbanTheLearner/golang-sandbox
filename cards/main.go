bd427ea1a89cabb3670b76443100ff1b04eef45e
Eshban Suleman <eshban9492@gmail.com>
2020-11-14 20:51:39 +0500

    Custom Card Declaration

--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	// var card string = "Ace of Spades" equivalent to card:= "Ace of Spades"
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
 
-	// card := "Ace of Spades"
-	// fmt.Println(card)
-
-	card := newCard()
-	fmt.Println(card)
+	cards.print()
 }
 
 func newCard() string {
