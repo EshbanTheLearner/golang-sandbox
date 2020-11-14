17889dc079d8f2bfa1e429401eca7bc215076d3c
Eshban Suleman <eshban9492@gmail.com>
2020-11-14 19:50:35 +0500

    Variables

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	// var card string = "Ace of Spades" equivalent to card:= "Ace of Spades"
+
+	// card := "Ace of Spades"
+	// fmt.Println(card)
+
+	card := newCard()
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
---
+++ language-basics/variables/main.go
@@ -0,0 +1,23 @@
+// Variable Declaration
+
+package main
+
+import "fmt"
+
+func main() {
+
+	/*
+		Data type should be told to the compiler in following delclaration syntax
+	*/
+
+	var name string = "Eshban"
+	fmt.Println(name)
+
+	/*
+		Following delclaration syntax doesn't require explicit data type
+	*/
+
+	name2 := "Suleman"
+	fmt.Println(name2)
+
+}
