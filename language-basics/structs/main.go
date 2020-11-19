39f550876bdef7e99d337cb7e9607dd7b6a744dc
Eshban Suleman <eshban9492@gmail.com>
2020-11-19 22:42:41 +0500

    Updating Structs

--- language-basics/structs/main.go
+++ language-basics/structs/main.go
@@ -8,8 +8,22 @@ type person struct {
 }
 
 func main() {
+
+	// Declaration Method 1
 	eshban := person{"Eshban", "Suleman"}
 	fmt.Println(eshban)
+
+	// Declaration Method 2
 	eshban2 := person{firstName: "Eshban", lastName: "Suleman"}
 	fmt.Println(eshban2)
+
+	// Declaration Method 3
+	var eshban3 person // Zero Values Assigned
+	fmt.Println(eshban3)
+	fmt.Printf("%+v", eshban3)
+
+	// Updating Struct
+	eshban3.firstName = "Eshban"
+	eshban3.lastName = "Suleman"
+	fmt.Printf("%+v", eshban3)
 }
