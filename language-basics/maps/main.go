aa3b5a6e122fda807c85bf4004f70340718a2ce1
Eshban Suleman <eshban9492@gmail.com>
2020-11-20 23:29:39 +0500

    Maps Basics

--- language-basics/maps/main.go
+++ language-basics/maps/main.go
@@ -1,12 +1,22 @@
 package main
 
-import "fmt"
-
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
 
-	fmt.Println(colors)
+	// Method 1
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	// Method 2
+
+	// var colors map[string]string
+
+	// method 3
+
+	// colors := make(map[string]string)
+	// colors["white"] = "#ffffff"
+
+	// fmt.Println(colors)
 }
