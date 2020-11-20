267288ab66c46fa726b8626d1646da64bd0aad43
Eshban Suleman <eshban9492@gmail.com>
2020-11-20 23:33:08 +0500

    Iterating over Maps

--- language-basics/maps/main.go
+++ language-basics/maps/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	// Method 1
@@ -19,4 +21,20 @@ func main() {
 	// colors["white"] = "#ffffff"
 
 	// fmt.Println(colors)
+
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+
+	// fmt.Println(colors)
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
